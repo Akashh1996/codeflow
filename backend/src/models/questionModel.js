48edf314386c0c45ed1039f80973146776b29f89
Akash Sapkota <akashh.sapkota@gmail.com>
2020-11-30 16:01:55 +0100

    deleted typescript

--- backend/src/models/questionModel.js
+++ backend/src/models/questionModel.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const questionSchema = new Schema({
-  question: { type: String },
+  questionTitile: { type: String },
+  questionDescription: { type: String },
+  date: { type: String },
+  user: { type: Schema.Types.ObjectId, ref: 'user' },
+  answers: [{
+    user: { type: Schema.Types.ObjectId, ref: 'users' },
+    answer: { type: String },
+    date: { type: String },
+    likes: { type: Number },
+    dislikes: { type: Number },
+
+  }],
+  likes: { type: Number },
+  dislikes: { type: Number },
+
 });
 
 module.exports = model('questions', questionSchema);
