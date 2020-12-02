0c3ef40edaea1856049c853c533381dce6754545
Akash Sapkota <akashh.sapkota@gmail.com>
2020-12-02 20:57:54 +0100

    fixed some css of forum page

--- frontend/src/Components/Forum/SecondaryNav.jsx
+++ frontend/src/Components/Forum/SecondaryNav.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import './secondaryNav.css';
+import { Link } from 'react-router-dom';
 
 function SecondaryNav() {
   return (
     <header>
       <div className="answer-types">
-        <h3>Recent</h3>
-        <h3>No Answer</h3>
-        <h3>Most Answered</h3>
-        <h3>Most Disliked</h3>
+        <Link to="/recent">Recent</Link>
+        <Link to="/any">No Answer</Link>
+        <Link to="/any">Most Answered</Link>
+        <Link to="/any">Most Disliked</Link>
       </div>
 
+      <div className="select-wrapper ">
+        <div className="select-options">
+          <select name="cars" id="cars" className="any">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+          </select>
+
+        </div>
+
+      </div>
     </header>
   );
 }
