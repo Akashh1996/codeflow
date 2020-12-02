db4a1ff4de0ecea00155848713e62cde40b8930e
Akash Sapkota <akashh.sapkota@gmail.com>
2020-12-02 11:35:31 +0100

    questionList done

--- backend/src/models/questionModel.js
+++ backend/src/models/questionModel.js
@@ -3,20 +3,14 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const questionSchema = new Schema({
-  questionTitile: { type: String },
+  questionTitle: { type: String },
   questionDescription: { type: String },
   date: { type: String },
-  user: { type: Schema.Types.ObjectId, ref: 'user' },
-  answers: [{
-    user: { type: Schema.Types.ObjectId, ref: 'users' },
-    answer: { type: String },
-    date: { type: String },
-    likes: { type: Number },
-    dislikes: { type: Number },
-
-  }],
+  owner: { type: Schema.Types.ObjectId, ref: 'user' },
+  answers: [{ type: Schema.Types.ObjectId, ref: 'answer' }],
   likes: { type: Number },
   dislikes: { type: Number },
+  tag: { type: String },
 
 });
 
