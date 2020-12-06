3efb328484b7a709c247d8e7aabb64c4265e5a8c
Akash Sapkota <akashh.sapkota@gmail.com>
2020-12-06 16:27:08 +0100

    login working with firebase google

--- frontend/src/redux/actions/Firebase/firebase.config.js
+++ frontend/src/redux/actions/Firebase/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyDorkY3hsSMoaIlIZgOoNFbfg8iRRCPrt0',
-  authDomain: 'codeflow-297720.firebaseapp.com',
-  databaseURL: 'https://codeflow-297720-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'codeflow-297720',
-  storageBucket: 'codeflow-297720.appspot.com',
-  messagingSenderId: '859499613036',
-  appId: '1:859499613036:web:c444559e60d11b7537c149',
+  apiKey: 'AIzaSyBc_15vWeY_0H1vZVO_G43wgbZgy0UXx2s',
+  authDomain: 'codeflow-ce514.firebaseapp.com',
+  projectId: 'codeflow-ce514',
+  storageBucket: 'codeflow-ce514.appspot.com',
+  messagingSenderId: '675363349869',
+  appId: '1:675363349869:web:d8f4954a410acc9e5fa664',
 };
 export default firebaseConfig;
