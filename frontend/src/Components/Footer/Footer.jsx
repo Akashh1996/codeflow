f09af97bced1317c7d4daaa963a96a2c2c6e7faf
Akash Sapkota <akashh.sapkota@gmail.com>
2020-12-16 08:54:13 +0100

    finalizado

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 function Footer() {
   return (
     <footer>
-      <a href="/" className="footer-elements">About Us</a>
+      <a href="/" className="footer-elements" id="about">About Us</a>
       <a href="/" className="footer-elements">Contact Us</a>
       <a href="/" className="footer-elements">Help</a>
       <a href="/" className="footer-elements">Blog</a>
