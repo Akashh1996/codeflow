7f06bcb7b5643c9df357697ac30a01ece9e75cfc
Akash Sapkota <akashh.sapkota@gmail.com>
2021-01-07 17:45:49 +0100

    added loading text in loading component

--- frontend/src/Components/Loading/Loading.jsx
+++ frontend/src/Components/Loading/Loading.jsx
@@ -2,22 +2,41 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
     width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
   },
-}));
+});
 
-export default function Loading() {
+export default function LinearDeterminate() {
   const classes = useStyles();
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
     <div className={classes.root}>
-      <LinearProgress color="primary" />
-      <LinearProgress color="primary" />
+      <h2 style={{
+        textAlign: 'center', color: '#0077CC', marginBottom: '10px', fontFamily: 'castoro',
+      }}
+      >
+        Loading...
+      </h2>
+      <LinearProgress variant="determinate" value={progress} />
     </div>
   );
 }
